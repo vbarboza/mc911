2e2c4a4e1d9c6987d652f2076255607290262334
Joao Mendes <ra102788@students.ic.unicamp.br>
2014-04-09 23:30:23 -0300

    atualizados .gitignore e Makefile. Acrescentado LlvmNot.java

---
+++ p2/pacote-gdb-v04/src/llvmast/LlvmNot.java
@@ -0,0 +1,10 @@
+package llvmast;
+public  class LlvmNot extends LlvmInstruction{
+
+    public LlvmNot(LlvmRegister lhs, LlvmType type, LlvmValue op1){
+    }
+
+    public String toString(){
+		return null;
+    }
+}
